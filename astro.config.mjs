c2ca8dcc6a34411ec7bde7b9f026cd34335343d0
clehma203 <chris@thecwlzone.com>
2025-03-20 12:59:51 -0600

    Add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://app.netlify.com/sites/zesty-bombolone-2e29a5/"
+  site: "https://app.netlify.com/sites/zesty-bombolone-2e29a5/",
+  integrations: [preact()]
 });
